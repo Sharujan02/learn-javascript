76e66e84ae010b52ba9c7a519680bd116112e0e2
Sharujan Sriramasothy <sharujan.sriramasothy@kcl.ac.uk>
2022-12-18 19:24:58 +0000

    "I don't know part 2"

--- 2. Practice time - part 1/5. Rendering an error message/index.js
+++ 2. Practice time - part 1/5. Rendering an error message/index.js
@@ -2,4 +2,8 @@
 // "Something went wrong, please try again" in the paragraph
 // that has the id="error".
 
+let errorId = document.getElementById("error")
 
+function errorMsg() {
+    errorId.textContent = "Something went wrong, please try again"
+}
---
+++ TestJavaScript/index.js
@@ -0,0 +1,15 @@
+let myPoints = 3
+
+function add3Points() {
+    myPoints += 3
+} 
+
+function remove1Point() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+
+console.log(myPoints)
